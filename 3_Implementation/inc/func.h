499a00d4a4fe12af0d69f8e47ce1636d8cf2ff0a
261718 <80752698+261718@users.noreply.github.com>
2021-04-16 01:24:18 +0530

    Add files via upload

--- 3_Implementation/inc/func.h
+++ 3_Implementation/inc/func.h
@@ -1,1 +1,26 @@
+#ifndef __FUNC_H__
+#define __FUNC_H__
 
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void ClearConsoleToColors(int ForgC, int BackC);
+void SetColor(int ForgC);
+void SetColorAndBackground(int ForgC, int BackC);
+void drawRectangle();
+void clearWindow();
+void window();
+void get_password(char* pass);
+void use_pass_field();
+void print_heading(const char st[]);
+int conf_record(char id[]);
+void add_student();
+void search_student();
+void mod_student();
+void gen_marksheet();
+void delete_student();
+void main_window();
+
+
+#endif
